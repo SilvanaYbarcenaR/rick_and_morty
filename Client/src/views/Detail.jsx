bd117f656200110a584139b8c98c2b59f7884b58
SilvanaYbarcenaR <silvana.ybarcena@gmail.com>
2023-09-07 23:56:57 -0500

    Changing geting character with promises.

--- Client/src/views/Detail.jsx
+++ Client/src/views/Detail.jsx
@@ -8,11 +8,9 @@ const Detail = () => {
 
     useEffect(() => {
         /* axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => { */
-        axios(`http://localhost:3001/rickandmorty/character/${id}}`).then(({ data }) => {
+        axios(`http://localhost:3001/rickandmorty/character/${id}`).then(({ data }) => {
            if (data.name) {
               setCharacter(data);
-           } else {
-              window.alert('No hay personajes con ese ID');
            }
         });
         return setCharacter({});
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,9 +1,10 @@
 const http = require("http");
+const getCharById = require("./Controllers/getCharById")
 const data = require("./utils/data.js")
 
 http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    if(req.url.includes("/rickandmorty/character")) {
+    /* if(req.url.includes("/rickandmorty/character")) {
         const id = req.url.split("/").at(-1);
         res.writeHead(200,
         {"Content-type": "application/json"})
@@ -12,5 +13,9 @@ http.createServer((req, res) => {
         })
         res.end(JSON.stringify(character));
         return;
+    } */
+    if(req.url.includes("/rickandmorty/character")) {
+        const id = req.url.split("/").at(-1);
+        getCharById(res, +id);
     }
 }).listen(3001, "localhost");
